c7fa3054d9e94b4001932a70eeca76d73c3efadb
Alexis Montas <montas77872@gmail.com>
2019-03-29 23:50:05 -0400

    TareaProgramacion3

---
+++ Registro/Registro/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registro.Models;
+namespace Registro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Bienvenida()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        public ActionResult OtherView()
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                Datos data = new Datos();
+                return View(data);
+            }
+
+        }
+        public ActionResult Resultado(Datos obj)
+        {
+            return View(obj);
+
+        }
+    }
+}
---
+++ Registro/Registro/Models/Datos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Registro.Models
+{
+    public class Datos
+    {
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public int Cedula { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [Range(15, 50, ErrorMessage = "Debe ser mayor de edad")]
+        public int Edad { get; set; }
+   
+        public int Telefono { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+
+        //Nombramos las referencias que usaremos en el ENUM
+        public GeneroRef Genero { get; set; }
+        public EstadoRef Estado { get; set; }
+        public HobbysRef Hobby { get; set; }
+    }
+
+    /*  enum es un tipo de datos de tipo de valor.
+    La enumeración se utiliza para declarar una lista de constantes enteras nombradas.*/
+    public enum GeneroRef
+    {
+        Masculino,
+        Femenino,
+        Desconocido
+    }
+    public enum EstadoRef
+    {
+        Casado,
+        Soltero,
+        Viudo,
+    }
+    public enum HobbysRef
+    {
+        Televison,
+        Deporte,
+        Musica,
+        Libros
+
+    }
+}
